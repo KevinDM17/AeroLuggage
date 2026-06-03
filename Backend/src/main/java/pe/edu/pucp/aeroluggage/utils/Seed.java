59596c9b9b9725cbcf0f5daf3456d6e0ace2c5a7
Adrián Arista <a20201550@pucp.edu.pe>
2026-06-03 14:23:19 -0500

    docker configurado

--- Backend/src/main/java/pe/edu/pucp/aeroluggage/utils/Seed.java
+++ Backend/src/main/java/pe/edu/pucp/aeroluggage/utils/Seed.java
@@ -1,6 +1,8 @@
 package pe.edu.pucp.aeroluggage.utils;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,9 +11,22 @@ import org.springframework.context.ConfigurableApplicationContext;
 import pe.edu.pucp.aeroluggage.AeroLuggageApplication;
 import pe.edu.pucp.aeroluggage.controller.DataLoaderController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api/seed")
 public class Seed {
+
+    private final DataLoaderController dataLoaderController;
+
+    public Seed(final DataLoaderController dataLoaderController) {
+        this.dataLoaderController = dataLoaderController;
+    }
+
+    @PostMapping
+    public void seed(@RequestParam(defaultValue = "false") final boolean clean) throws IOException {
+        dataLoaderController.seed(clean);
+    }
 
     public static void main(String[] args) {
 
